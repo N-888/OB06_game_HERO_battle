6e2216e0e07932a343ef4087c5e0cb8892377243
N-888 <mamambell@gmail.com>
2025-07-08 02:09:04 +0300

    class Game

--- main.py
+++ main.py
@@ -10,3 +10,23 @@ class Hero:
 
     def is_alive(self):
         return self.health > 0
+
+
+class Game:
+    def __init__(self, player_name):
+        self.player = Hero(player_name)
+        self.computer = Hero("Компьютер")
+
+    def start(self):
+        current_turn = 0
+        while self.player.is_alive() and self.computer.is_alive():
+            if current_turn % 2 == 0:
+                self.player.attack(self.computer)
+                print(f"{self.player.name} атаковал! У {self.computer.name} осталось {self.computer.health} здоровья")
+            else:
+                self.computer.attack(self.player)
+                print(f"{self.computer.name} атаковал! У {self.player.name} осталось {self.player.health} здоровья")
+            current_turn += 1
+
+        winner = self.player.name if self.player.is_alive() else self.computer.name
+        print(f"\nПобедитель: {winner}!")
